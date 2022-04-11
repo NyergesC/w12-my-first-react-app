69352377719e54fc1a732b50b76b1616a65717e5
NyergesC <cintia.nyerges@gmail.com>
2022-04-11 16:52:57 +0200

    constructor

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Beers from './components/Beers'
 import data from './data'
+import Header from './components/Header'
 
 /* console.log(<Beers list={data.cards}/>); */
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header title={data.logo}/>
     <Beers list={data.cards}/>
   </React.StrictMode>,
   document.getElementById('root')
